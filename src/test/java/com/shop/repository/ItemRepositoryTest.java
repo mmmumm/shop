868998ca03c306d84ef3f8368936fe0d1313353e
mmmumm <gkdlduas@gmail.com>
2022-07-05 20:43:25 +0900

    220705-4

--- src/test/java/com/shop/repository/ItemRepositoryTest.java
+++ src/test/java/com/shop/repository/ItemRepositoryTest.java
@@ -10,12 +10,10 @@ import org.springframework.test.context.TestPropertySource;
 
 import java.time.LocalDateTime;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 import java.util.List;
 
 @SpringBootTest
-@TestPropertySource(locations="classpath:application-test.properties")
+@TestPropertySource(locations="classpath:application.properties")
 class ItemRepositoryTest {
     @Autowired
     ItemRepository itemRepository;
@@ -34,7 +32,7 @@ class ItemRepositoryTest {
         Item savedItem = itemRepository.save(item);
         System.out.println(savedItem.toString());
     }
-    public void createItmeList(){
+    public void createItemList(){
         for(int i=1;i<=10;i++){
             Item item = new Item();
             item.setItemNm("테스트 상품"+i);
@@ -50,8 +48,18 @@ class ItemRepositoryTest {
     @Test
     @DisplayName("상품명 조회 테스트")
     public void findByItemNmTest(){
-        this.createItmeList();
+        this.createItemList();
         List<Item> itemList = itemRepository.findByItemNm("테스트 상품1");
+        for (Item item:itemList) {
+            System.out.println(item.toString());
+        }
+    }
+    @Test
+    @DisplayName("상품명, 상품상세설명 or 테스트")
+    public void findByItemNmOrItemDetailTest(){
+        this.createItemList();
+        List<Item> itemList =
+                itemRepository.findByItemNmOrItemDetail("테스트 상품1","테스트 상품 상세 설명5");
         for (Item item:itemList) {
             System.out.println(item.toString());
         }
