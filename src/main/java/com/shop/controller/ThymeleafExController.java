217f90d48acb005af05239b5aacde3fe5667ba76
mmmumm <53213811+mmmumm@users.noreply.github.com>
2022-07-06 23:07:26 +0900

    220706-5

--- src/main/java/com/shop/controller/ThymeleafExController.java
+++ src/main/java/com/shop/controller/ThymeleafExController.java
@@ -66,5 +66,14 @@ public class ThymeleafExController {
     public String thymeleafExample05(){
         return "thymeleafEx/thymeleafEx05";
     }
-
+    @GetMapping(value = "/ex06")
+    public String thymeleafExample06(String param1, String param2,Model model){
+        model.addAttribute("param1",param1);
+        model.addAttribute("param2", param2);
+        return "thymeleafEx/thymeleafEx06";
+    }
+    @GetMapping(value = "/ex07")
+    public String thymeleafExample07(){
+        return "thymeleafEx/thymeleafEx07";
+    }
 }
