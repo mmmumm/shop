a02974745dbe3246deaf04f635694e7f17773f42
mmmumm <53213811+mmmumm@users.noreply.github.com>
2022-07-08 21:45:55 +0900

    6-2-2
    264

--- src/main/java/com/shop/entity/Item.java
+++ src/main/java/com/shop/entity/Item.java
@@ -1,6 +1,7 @@
 package com.shop.entity;
 
 import com.shop.constant.ItemSellStatus;
+import com.shop.dto.ItemFormDto;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
@@ -13,7 +14,7 @@ import java.time.LocalDateTime;
 @Getter
 @Setter
 @ToString
-public class Item {
+public class Item extends BaseEntity{
     @Id
     @Column(name="item_id")
     @GeneratedValue(strategy = GenerationType.AUTO)
@@ -46,4 +47,11 @@ public class Item {
 //            inverseJoinColumns = @JoinColumn(name="item_id")
 //    )
 //    private List<Member> member;
+    public void updateItem(ItemFormDto itemFormDto){
+        this.itemNm =itemFormDto.getItemNm();
+        this.price=itemFormDto.getPrice();
+        this.stockNumber = itemFormDto.getStockNumber();
+        this.itemDetail = itemFormDto.getItemDetail();
+        this.itemSellStatus = itemFormDto.getItemSellStatus();
+    }
 }
